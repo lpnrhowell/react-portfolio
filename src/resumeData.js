1885bdd90bc473e18ea6ef461d80888d9db37ea4
“lpnrhowell” <lpnrhowell@gmail.com>
2021-08-24 22:24:45 -0400

    added portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,84 @@
+let resumeData = {
+	imagebaseurl: "https://joshkuruvilla512.github.io/",
+	name: "Robert Howell",
+	role: "Full Stack Developer",
+	linkedinId: "Your LinkedIn Id",
+	roleDescription: "",
+	socialLinks: [
+		{
+			name: "linkedin",
+			url: "https://www.linkedin.com/in/robert-howell-b28a5a207/",
+			className: "fa fa-linkedin",
+		},
+		{
+			name: "github",
+			url: "http://github.com/lpnrhowell",
+			className: "fa fa-github",
+		},
+	],
+	aboutme:
+		"I am a Full Stack Developer well-versed in React, Javascript, and Graphql ",
+	address: "Miami, FL",
+	website: "https://react-portfolio-ut.herokuapp.com/",
+	education: [
+		{
+			UniversityName: "University of Miami",
+			specialization: "Full Stack Development",
+			MonthOfPassing: "August",
+			YearOfPassing: "2021",
+			Achievements: "Certificate",
+		},
+		{
+			UniversityName: "University of Miami",
+			specialization: "Communication",
+			MonthOfPassing: "December",
+			YearOfPassing: "2000",
+			Achievements: "Bachelors",
+		},
+	],
+	work: [
+		{
+			CompanyName: "UM Cancer Center",
+			specialization: "LPN",
+			MonthOfLeaving: "July",
+			YearOfLeaving: "2018-November 2019",
+			Achievements: "",
+		},
+	],
+	skillsDescription: "",
+	skills: [
+		{
+			skillname: "HTML5",
+		},
+		{
+			skillname: "CSS",
+		},
+		{
+			skillname: "Reactjs",
+		},
+	],
+	portfolio: [
+		{
+			name: "Ryders Rally",
+			description: "MC club events",
+			imgurl: "./images/ryderRally.png",
+			imglink: "https://rydersrally.herokuapp.com/",
+		},
+		{
+			name: "Note Taker",
+			description: "Create notes",
+			imgurl: "./images/notesApp.png",
+			imglink: "https://notetakingapp1.herokuapp.com/",
+		},
+		{
+			name: "Fitness Tracker",
+			description: "Tracke workouts",
+			imgurl: "./images/fitnessTracker.png",
+			imglink:
+				"https://fitnesstracker1213.herokuapp.com/?id=61226fcc288aa70015d06741",
+		},
+	
+	],
+};
+
+export default resumeData
